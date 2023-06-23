fc793e7c0f77d85d6682cdc090b0808e546f944d
salenctawer <salenctawer@gmail.com>
2023-06-24 01:30:08 +0500

    change sass package

--- src/components/app/AppSidebar/AppSidebar.tsx
+++ src/components/app/AppSidebar/AppSidebar.tsx
@@ -1,6 +1,14 @@
+'use client'
+
 import { FC } from "react"
+import { useAppSelector } from "@/redux/hooks"
+
+import styles  from './AppSidebar.module.scss'
 
 
 export const AppSidebar: FC = () => {
-    return <div>Sidebar</div>
+    const sidebarAuthTabs = useAppSelector(state => state.sidebar.sidebarAuthTabs)
+    const sidebarNonAuthTabs = useAppSelector(state => state.sidebar.sidebarNonAuthTabs)
+
+    return <div className={styles.appSidebar}>fsdfsdf</div>
 }
