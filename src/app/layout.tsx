7e93a2185edec2d060ff942ad92a38aefaad482f
salenctawer <salenctawer@gmail.com>
2023-07-12 22:36:36 +0500

    footer create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Providers } from "@/redux/provider";
 
 import AppSidebar from '@/components/app/AppSidebar/AppSidebar';
 import AppHeader from '@/components/app/AppHeader/AppHeader';
+import AppFooter from '@/components/app/AppFooter/AppFooter';
 
 import ThemeBuilder from '@/styles/themeBuilder';
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             <AppHeader />
             <AppSidebar />
             {children}
+            <AppFooter />
           </ThemeBuilder>
         </Providers>
       </body>
---
+++ src/components/app/AppFooter/AppFooter.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react"
+
+
+export const AppFooter: FC = () => {
+    return <div>TODO footer</div>
+}
+
+export default AppFooter
