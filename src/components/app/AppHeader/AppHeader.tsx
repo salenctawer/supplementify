2781f00f332d4fd191e46bd6de8a16c0cce78f68
salenctawer <salenctawer@gmail.com>
2023-09-17 10:48:43 +0500

    update sidebar drawer

--- src/components/app/AppHeader/AppHeader.tsx
+++ src/components/app/AppHeader/AppHeader.tsx
@@ -2,7 +2,7 @@
 
 import React, { FC, useEffect, useMemo, useState } from "react"
 
-import { Box, AppBar, Toolbar, Typography, Button, Avatar, useTheme } from "@mui/material"
+import { Box, AppBar, Toolbar, Typography, Button, Avatar, useTheme, SwipeableDrawer, List, ListItem } from "@mui/material"
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { fetchUserInfo, fetchAuthUrl } from "@/redux/slices/userSlice";
 import { useAuth } from "@/hooks/useAuth";
@@ -14,6 +14,7 @@ import { ModeTypes } from "@/types/EnumsData";
 
 import styles from './AppHeader.module.scss'
 import useMedia from "@/styles/useMedia";
+import { IconComponent } from "@/components/ui/IconComponent/IconComponent";
 
 
 export const AppHeader: FC = () => {
@@ -25,6 +26,7 @@ export const AppHeader: FC = () => {
     const router = useRouter()
     const [scrollTop, setScrollTop] = useState(0);
     const { mdSize } = useMedia()
+    const [isOpenDrawer, setIsOpenDrawer] = useState(false)
 
     useEffect(() => {
         if (isAuth) {
@@ -72,44 +74,87 @@ export const AppHeader: FC = () => {
         return router.push(data.payload)
     }
 
+    const toggleDrawer = (state: boolean) => {
+        setIsOpenDrawer(state)
+    }
+
 
 
     return (
     <Box className={`${scrollTop > 64 ? styles.headerFixed : styles.header}`} >
             <AppBar position="static" sx={{ borderRadius: theme.shape }}>
-                <Toolbar>
+                <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
                     {
                         mdSize ? <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                                     Supplementify
                                 </Typography>
                                 :
-                        <div>fsdfsdfsd</div>
+                            <SidebarDrawer toggleDrawer={toggleDrawer} isOpen={isOpenDrawer}/>
                     }
-                    <DarkModeSwitch
-                        style={{ margin: 'auto 16px auto 0' }}
-                        checked={isDarkMode}
-                        onChange={toggleMode}
-                        size={24}
-                    />
-                    {
-                        userInfo ?
-                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                <Avatar src={userInfo.images[0].url} />
-                                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                    {userInfo.display_name}
-                                    <Button color="inherit" onClick={onLogoutClick}>Logout</Button>
+                    <Box sx={{display: 'flex'}}>
+                        <DarkModeSwitch
+                            style={{ margin: 'auto 16px auto 0' }}
+                            checked={isDarkMode}
+                            onChange={toggleMode}
+                            size={24}
+                        />
+                        {
+                            userInfo ?
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <Avatar src={userInfo.images[0].url} />
+                                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                        {userInfo.display_name}
+                                        <Button color="inherit" onClick={onLogoutClick}>Logout</Button>
+                                    </Box>
                                 </Box>
-                            </Box>
-                            : <Button color="inherit" onClick={onLoginClick}>Login</Button>
-                    }
+                                : <Button color="inherit" onClick={onLoginClick}>Login</Button>
+                        }
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Box>
     )
 }
 
-const SidebarDrawer = () => {
-    
+interface SidebarDrawerProps {
+    toggleDrawer: (state: boolean) => void
+    isOpen: boolean
 }
+
+const SidebarDrawer: FC<SidebarDrawerProps> = (props) => {
+    const sidebarTabs = useAppSelector(state => state.sidebar.sidebarAuthTabs)
+    const {toggleDrawer, isOpen} = props
+
+    return (
+        <div>   
+            <Button onClick={() => toggleDrawer(true)}>
+                <IconComponent iconName="MenuOutlined"/>
+            </Button>
+            <SwipeableDrawer
+                anchor="left"
+                open={isOpen}
+                onClose={() => toggleDrawer(false)}
+                onOpen={() => toggleDrawer(true)}
+            >
+                <Box
+                    role="presentation"
+                    onClick={() => toggleDrawer(false)}
+                    onKeyDown={() => toggleDrawer(false)}
+                >
+                    <List>
+                        {
+                            sidebarTabs.map((item) => (
+                                <ListItem key={item.name}>
+                                    {item.name}
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+                </Box>
+            </SwipeableDrawer>
+        </div>
+    )
+}
+
 
 export default AppHeader
